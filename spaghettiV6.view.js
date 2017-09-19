913631a4d56053bb4fee136855a9ad7e34f17236
SergiBaucells <sergibaucells@iesebre.com>
2017-09-19 08:52:58 +0200

    20170919

---
+++ spaghettiV6.view.js
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+</head>
+<body>
+<header>
+
+   const markup = '
+    <div>
+        <h2>
+            ${person.name}
+        </h2>
+        <p class="location">${person.location}</p>
+        <p class="bio">${person.bio}</p>
+    </div>
+    ';
+
+</header>
+</body>
+</html>
